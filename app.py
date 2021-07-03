6e713b517a4997398f7e9ac80331a9d64b0bf4ce
Nayanjyoti Rabha <61662840+Git-Nayanjyoti@users.noreply.github.com>
2021-07-04 01:51:12 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,135 @@
+import numpy as np
+import streamlit as st
+import torch
+import torchvision.transforms as T
+from PIL import Image
+from torchvision.utils import make_grid, save_image
+
+from model import RestorationModel
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+CHECKPOINT = "https://github.com/Git-Nayanjyoti/image-inpainter/releases/download/v-0.0.1/weights_sun_4_jul_2021.ckpt"
+CHECKPOINT2 = ""
+SIZE = 256
+
+# MODEL_WEIGHTS_DEPLOYMENT_URL = ''
+
+# Constants for sidebar dropdown
+SIDEBAR_OPTION_PROJECT_INFO = "Show Project Info"
+SIDEBAR_OPTION_ENCODER_DECODER = "Auto Encoder Decoder"
+SIDEBAR_OPTION_PIX2PIX = "PIX2PIX GAN"
+
+SIDEBAR_OPTIONS = [SIDEBAR_OPTION_PROJECT_INFO, SIDEBAR_OPTION_ENCODER_DECODER, SIDEBAR_OPTION_PIX2PIX]
+
+preprocess_fn = A.Compose([
+    A.ToFloat(max_value=255.0),
+    ToTensorV2(p=1.0)
+])
+
+dropout_fn = A.CoarseDropout(max_holes=1, min_holes=1, p=1.0, max_width=50, max_height=50)
+resize_fn  = A.Resize(SIZE, SIZE, p=1.0)
+
+def make_holes(image):
+    image = np.array(image)
+    with st.spinner(" Genrating patches over image ..."):
+        aug_image = dropout_fn(image=image)['image']
+    image = aug_image.astype(np.uint8)
+    image = Image.fromarray(image)
+    st.markdown("### Image after adding patches")
+    st.image(image, caption='Image with patches')
+    return aug_image
+
+def resize_image(uploaded_image):
+    image = np.array(uploaded_image)
+    image = resize_fn(image=image)['image']
+
+    display_image = image.astype(np.uint8)
+    display_image = Image.fromarray(display_image)
+    st.title("Here is the image you've uploaded")
+    st.image(image, caption='Uploaded Image')
+    return image
+
+
+@ st.cache()
+def load_model():
+    model = RestorationModel.load_from_checkpoint(CHECKPOINT)
+    model.eval()
+    return model
+
+
+# Inference function - TODO this could probably be improved ...
+@ torch.no_grad()
+def do_predict(image):
+    image = np.array(image)
+    with st.spinner("🏃‍♂️ Getting the latest model weights ..."):
+        model = load_model()
+    st.success("🚀 Model Weights Loaded Successfully !")
+    image = preprocess_fn(image=image)['image']
+    image = image.unsqueeze(0)
+    with st.spinner("🏃‍♂️ Doing the Math 🤓 ..."):
+        # add batch dimension to image
+        results = model(image)
+    st.success("🚀 Predictions Generated !")
+    results = results.sigmoid()
+    results = make_grid(results, normalize=True).permute(1, 2, 0).data.cpu().numpy()
+    results *= 255.0
+    results = results.astype(np.uint8)
+    results = A.functional.adjust_brightness_torchvision(results, factor=1.2)
+    results = A.functional.adjust_contrast_torchvision(results, factor=0.8)
+    return Image.fromarray(results)
+
+@st.cache(show_spinner=False)
+def get_file_content_as_string(path):
+    url = 'https://raw.githubusercontent.com/Git-Nayanjyoti/image-inpainter/' + path
+    response = urllib.request.urlopen(url)
+    return response.read().decode("utf-8")
+
+
+# fmt: off
+def main():
+    title = """
+    # Image Inpainter
+    """
+    
+    st.markdown(title)
+    st.write(" --------- ")
+
+    st.sidebar.warning("Please upload SINGLE-person images. For best results, also center the person in the image")
+    st.sidebar.write(" -------- ")
+    st.sidebar.title("Explore the Following")
+
+    app_mode = st.sidebar.selectbox("Please select from the following", SIDEBAR_OPTIONS)
+
+    if app_mode == SIDEBAR_OPTION_PROJECT_INFO:
+        st.sidebar.write(" ------- ")
+        st.sidebar.success("Project information showing on the right!")
+        st.write(get_file_content_as_string("Project_Info.md"))
+
+    elif app_mode == SIDEBAR_OPTION_ENCODER_DECODER:
+        st.write("AUTO ENCODER DECODER")
+        uploaded_image = st.file_uploader("Upload An Image", type=["jpg", "png", "jpeg"])
+
+        if uploaded_image is not None:
+           image = Image.open(uploaded_image)
+           image = resize_image(image)
+           image = make_holes(image)
+           predictions = do_predict(image)
+           st.image(predictions, caption='restored image')
+    else:
+        st.write("PIX2PIX GAN")
+        uploaded_image_gan = st.file_uploader("Upload An Image", type=["jpg", "png", "jpeg"])
+
+        if uploaded_image_gan is not None:
+            image_gan = Image.open(uploaded_image_gan)
+            image_gan = resize_image(image_gan)
+            image_gan = make_holes(image_gan)
+           
+
+
+
+    
+
+if __name__ == "__main__":
+    # run the app
+    main()
---
+++ model.py
@@ -0,0 +1,110 @@
+from torch import nn
+import math
+from torch import optim
+import torch.nn.functional as F
+import pytorch_lightning as pl
+
+class RestorationAutoEncoder(nn.Module):
+    def __init__(self, num_layers=10, num_features=64):
+        super(RestorationAutoEncoder, self).__init__()
+        self.num_layers = num_layers
+
+        conv_layers = []
+        deconv_layers = []
+
+        conv_layers.append(
+            nn.Sequential(
+                nn.Conv2d(3, num_features, kernel_size=3, stride=2, padding=1),
+                nn.BatchNorm2d(num_features),
+                nn.ReLU(inplace=True)
+            )
+        )
+        for i in range(num_layers - 1):
+            conv_layers.append(
+                nn.Sequential(
+                    nn.Conv2d(num_features, num_features, kernel_size=3, padding=1),
+                    nn.BatchNorm2d(num_features),
+                    nn.ReLU(inplace=True)
+                )
+            )
+
+        for i in range(num_layers - 1):
+            deconv_layers.append(
+                nn.Sequential(
+                    nn.ConvTranspose2d(num_features, num_features, kernel_size=3, padding=1),
+                    nn.BatchNorm2d(num_features),
+                    nn.ReLU(inplace=True)
+                )
+            )
+        deconv_layers.append(
+            nn.ConvTranspose2d(num_features, 3, kernel_size=3, stride=2, padding=1, output_padding=1),
+        )
+
+        self.conv_layers   = nn.Sequential(*conv_layers)
+        self.deconv_layers = nn.Sequential(*deconv_layers)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+
+        conv_feats = []
+        for i in range(self.num_layers):
+            x = self.conv_layers[i](x)
+            if (i + 1) % 2 == 0 and len(conv_feats) < math.ceil(self.num_layers / 2) - 1:
+                conv_feats.append(x)
+
+        conv_feats_idx = 0
+        for i in range(self.num_layers):
+            x = self.deconv_layers[i](x)
+            if (i + 1 + self.num_layers) % 2 == 0 and conv_feats_idx < len(conv_feats):
+                conv_feat = conv_feats[-(conv_feats_idx + 1)]
+                conv_feats_idx += 1
+                x = x + conv_feat
+                x = self.relu(x)
+
+        x += residual
+        x = self.relu(x)
+
+        return x
+
+
+class RestorationModel(pl.LightningModule):
+    def __init__(self, lr: float, num_layers: int = 10, num_features: int = 64):
+        super(RestorationModel, self).__init__()
+        self.save_hyperparameters()
+        self.model = RestorationAutoEncoder(
+            num_layers=self.hparams.num_layers, num_features=self.hparams.num_features
+        )
+
+    def forward(self, xb):
+        "forward method for nn.Module"
+        return self.model(xb)
+
+    def training_step(self, batch, batch_idx):
+        "Computer on single training step"
+        # unpack batch
+        cln_ims, nsy_ims = batch
+
+        # restore images given noisy images to the model as Input
+        outputs = self.forward(nsy_ims)
+
+        # compare the restored images with the clean Images
+        loss = F.binary_cross_entropy_with_logits(input=outputs, target=cln_ims)
+
+        self.log_dict(
+            dict(train_loss=loss), prog_bar=True, on_epoch=True, on_step=False
+        )
+        return loss
+
+    def configure_optimizers(self):
+        "steps up the optimizer and scheduler for the experiment"
+        opt = optim.AdamW(
+            self.model.parameters(), lr=self.hparams.lr, weight_decay=1e-05
+        )
+        sch = optim.lr_scheduler.CosineAnnealingLR(opt, T_max=self.trainer.max_epochs)
+        return [opt], [sch]
+
+
+if __name__ == "__main__":
+    state = "checkpoint.ckpt"
+    model = RestorationModel.load_from_checkpoint(state)
